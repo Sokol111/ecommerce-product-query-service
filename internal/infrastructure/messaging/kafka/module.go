14a68d043efb886e064e8f35f97af9805b9eaf8c
Sokol111 <igorsokol111@gmail.com>
2025-11-26 19:54:33 +0100

    u

--- internal/infrastructure/messaging/kafka/module.go
+++ internal/infrastructure/messaging/kafka/module.go
@@ -1,6 +1,7 @@
 package kafka
 
 import (
+	"github.com/Sokol111/ecommerce-commons/pkg/messaging/kafka/avro/mapping"
 	"github.com/Sokol111/ecommerce-commons/pkg/messaging/kafka/consumer"
 	"github.com/Sokol111/ecommerce-product-service-api/events"
 	"go.uber.org/fx"
@@ -8,6 +9,16 @@ import (
 
 func Module() fx.Option {
 	return fx.Options(
-		consumer.RegisterHandlerAndConsumer("product-events", newProductHandler, events.DefaultTypeMapping),
+		consumer.RegisterHandlerAndConsumer("product-events", newProductHandler),
+		fx.Invoke(registerSchemas),
 	)
+}
+
+func registerSchemas(tm *mapping.TypeMapping) error {
+	for _, reg := range events.TypeRegistrations {
+		if err := tm.Register(reg.GoType, reg.SchemaJSON, reg.SchemaName, reg.Topic); err != nil {
+			return err
+		}
+	}
+	return nil
 }

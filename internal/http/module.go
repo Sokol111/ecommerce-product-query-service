5e50de35e8afc7e623af2cf16fad70eec644bd07
Sokol111 <igorsokol111@gmail.com>
2025-12-10 20:40:59 +0100

    u

--- internal/http/module.go
+++ internal/http/module.go
@@ -1,37 +1,22 @@
 package http
 
 import (
-	"fmt"
-
 	"github.com/Sokol111/ecommerce-product-query-service-api/gen/httpapi"
-	"github.com/getkin/kin-openapi/openapi3"
-	"github.com/gin-gonic/gin"
 	"go.uber.org/fx"
-	"go.uber.org/zap"
 )
 
 func NewHttpHandlerModule() fx.Option {
 	return fx.Options(
 		fx.Provide(
 			newProductHandler,
-			func(ssi httpapi.StrictServerInterface) httpapi.ServerInterface {
-				return httpapi.NewStrictHandler(ssi, nil)
-			},
+			newStrictHandler,
 			// Provide OpenAPI spec - gin module will auto-register validation middleware
-			newOpenAPISpec,
+			httpapi.GetSwagger,
 		),
-		fx.Invoke(registerRoutes),
+		fx.Invoke(httpapi.RegisterHandlers),
 	)
 }
 
-func newOpenAPISpec(log *zap.Logger) (*openapi3.T, error) {
-	swagger, err := httpapi.GetSwagger()
-	if err != nil {
-		return nil, fmt.Errorf("failed to load OpenAPI spec: %w", err)
-	}
-	return swagger, nil
-}
-
-func registerRoutes(engine *gin.Engine, serverInterface httpapi.ServerInterface) {
-	httpapi.RegisterHandlers(engine, serverInterface)
+func newStrictHandler(ssi httpapi.StrictServerInterface) httpapi.ServerInterface {
+	return httpapi.NewStrictHandler(ssi, nil)
 }
